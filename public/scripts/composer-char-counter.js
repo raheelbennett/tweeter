fe6213282b71539d38600a0da398ecef64be506d
Raheel Bennett <raheel.bennett@telus.com>
2022-12-07 00:37:38 -0700

    add main container, tweet container, shadow and template for tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,19 @@
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-//with help from mentor. This is not my own code.
-$("#tweet-text").on("input", function () {
-  const counter = $(this).val().length;
-  const remaining = 140 - counter;
-  // console.log(this.text())
-  $(".counter").text(remaining)  
+  //with help from mentor. This is not my own code.
+  $("#tweet-text").on("input", function () {
+    const counter = $(this).val().length;
+    const remaining = 140 - counter;
+    $(".counter").text(remaining)
 
-  if (remaining < 0) {
-    $(".counter").css("color", "red")
-  } else {
-    $(".counter").css("color", "black")
-  }
+    if (remaining < 0) {
+      $(".counter").css("color", "red")
+    } else {
+      $(".counter").css("color", "black")
+    }
 
   })
 
-  
 });
